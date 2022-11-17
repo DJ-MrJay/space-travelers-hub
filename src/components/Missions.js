690c65bf3db7487e7fd5c3b96a25c670ec71f95e
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-11-17 21:32:08 +0300

    Edit styles

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -17,8 +17,7 @@ const Missionlist = () => {
       <div className="grid border border-zinc-300 divide-x-[1px] divide-zinc-300 grid-cols-7">
         <div className="col-span-1 font-bold p-2">Mission</div>
         <div className="col-span-4 font-bold p-2">Description</div>
-        <div className="col-span-1 font-bold p-2 text-center">Status</div>
-        <div className="col-span-1" />
+        <div className="col-span-2 font-bold p-2 text-center">Status</div>
       </div>
       <ul className="mission-ul">
         {mission.map((item) => (
